4f0075a6d1c5cc7039219a203eb65647c0639574
Noah-Li2012 <noahli2012@qq.com>
2025-09-21 11:09:20 +0800

    Create meme.py

---
+++ meme.py
@@ -0,0 +1,10 @@
+import keyboard
+
+keyboard.block_key("ctrl")
+# I'M OUT OF CONTROL
+
+keyboard.block_key("escape")
+# I HAVE NO ESCAPE
+
+keyboard.block_key("space")
+# PLEASE JUST GIVE ME SOME SPACE
